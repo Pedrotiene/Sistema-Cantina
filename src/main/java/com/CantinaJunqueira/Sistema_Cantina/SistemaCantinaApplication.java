5ce340a410383b398445ad8e4eac5c9b2351be44
Pedro Pasquini Tiene <pedrotienep@gmail.com>
2026-01-15 14:53:40 -0300

    Cria Projeto/Backend Spring

---
+++ src/main/java/com/CantinaJunqueira/Sistema_Cantina/SistemaCantinaApplication.java
@@ -0,0 +1,13 @@
+package com.CantinaJunqueira.Sistema_Cantina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaCantinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaCantinaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CantinaJunqueira/Sistema_Cantina/SistemaCantinaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CantinaJunqueira.Sistema_Cantina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaCantinaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
